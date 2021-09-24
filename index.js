5e15ab3409fe2f97eb73d62f3a03e89658b5ef8d
shaurya2187 <shaurya.verma@ajackus.com>
2021-09-24 16:38:23 +0530

    added function to call

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+function getRequest(res)
+    {
+        const request = require('request')
+        request('https://time.com/', function(error, response, body)
+        {
+            // console.error('error:', error)
+            // console.log('body:', body)
+            if(body)
+                matchData(body)
+        })
+    }
+
+    function matchData(data)
+    {
+        const result = data.match('/homepage\-module')
+        // console.log(result)
+        res.send(result)
+    }
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => getRequest(res))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
